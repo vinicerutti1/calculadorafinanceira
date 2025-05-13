9cdc82d44908ca30d4524c7acd43ecbe8c200a4a
vinicerutti1 <vinicerutti0@gmail.com>
2025-05-12 21:12:51 -0300

    v1

---
+++ app.js
@@ -0,0 +1,11 @@
+const db = new PouchDB('Finances');
+const form = document.getElementById('RecordForm');
+const list = document.getElementById('RecordsList');
+const TotalDaily = document.getElementById('DailySum');
+const Total = document.getElementById('TotalSum');
+
+form.addEventListener("submit", async (e) => {
+
+
+}
+);
